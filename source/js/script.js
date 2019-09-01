1c8d822512368e5a841a617f7f4d83189a797d7f
DVNst <dvn-studio@mail.ru>
2019-09-01 22:23:42 +0300

    add block contacts and other

--- source/js/script.js
+++ source/js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+(function() {
+  document.body.onload = function() {
+    var noJS = document.querySelectorAll('.no-js');
+    var mainNavToggle = document.querySelector('.main-nav__toggle');
+
+    noJS.forEach(function(item) {
+      item.classList.remove('no-js');
+    });
+    mainNavToggle.classList.add('main-nav__toggle--closed');
+
+    mainNavToggle.addEventListener('click', function(e) {
+      e.preventDefault;
+      mainNavToggle.classList.toggle('main-nav__toggle--closed');
+    });
+  }
+})();
